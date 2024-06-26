43338dffdb763e3b85097fa4c93f4a761f2b4ba7
SriMoorthy <srimoorthy27@gmail.com>
2024-06-26 16:01:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function fibonacci(num) {
-    if (num <= 0) {
-        return 0;
-    }
-    if (num === 1) {
-        return 1;
-    }
-    let a = 0, b = 1, c;
-    for (let i = 2; i <= num; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
+  if (num <= 0) {
+      return 0;
+  }
+  if (num === 1) {
+      return 1;
+  }
+  let a = 0, b = 1, c;
+  for (let i = 2; i <= num; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+  }
+  return a;
+  
 }
 
 module.exports = fibonacci;
